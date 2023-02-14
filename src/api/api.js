f7039463767ee54f31a817c96b6d0dd61e965164
Zeljko <zeljkomaric95@gmail.com>
2023-02-14 11:16:58 -0700

    profile.js hardcoded

--- src/api/api.js
+++ src/api/api.js
@@ -1,1 +1,15 @@
 //This file will be for all functions making calls to the API i.e fetch, post etc.
+
+// export async function getProfile() {
+//     try {
+//       const res = await fetch(`${APIURL}/users/me`, {
+//         headers: getHeaders(),
+//         method: "GET",
+//       });
+//       const data = await res.json();
+//       console.log(data.data.posts);
+//       return data.data;
+//     } catch (error) {
+//       throw error;
+//     }
+//   }
