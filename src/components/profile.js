9c6393e02b66bade755e3dd3cea6b444ffeffe90
Zeljko <zeljkomaric95@gmail.com>
2023-02-18 08:22:38 -0700

    Profile.js under construction

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 import { BASE_URL, getUserProfile } from "../api/api";
 
 const Profile = ({ setOnline }) => {
   const [profile, setProfile] = useState(null);
   const [loading, setLoading] = useState(true);
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
   
   const currentToken = localStorage.getItem("auth_token");
 
@@ -24,10 +24,10 @@ const Profile = ({ setOnline }) => {
     isLoggedIn();
   }, [currentToken]);
 
-  const logout = () => {
-    setOnline(false);
-    navigate('/');
-  };
+  // const logout = () => {
+  //   setOnline(false);
+  //   navigate('/');
+  // };
   
   if (loading) {
     return <h2>Loading...</h2>;
@@ -37,51 +37,18 @@ const Profile = ({ setOnline }) => {
     return <h2>You are not logged in!</h2>;
   }
 
+  
   return (
     <div className="profile-container">
-      <div>
-        <h1>Profile</h1>
-        <h3>Welcome {profile.username}!</h3>
-        <p>Username: {profile.username}</p>
-        <p>User ID: {profile.id}</p>
         <div>
-          <h4>Routines:</h4>
-          {profile.routines?.map((routine, index) => (
-            <div key={routine._id} className="profile-routine">
-              <h4>Name: {routine.name}</h4>
-              <p>Description: {routine.description}</p>
-              <p>Activities: {routine.activities.map((activity, index) => (
-                <span key={activity._id} className="profile-activity">{activity.name}</span>
-              ))}</p>
+          <h1>MY ROUTINES</h1>
+          {profile.routine?.map((routine, index) => (
+            <div key={routine._id} className="profile-my-routine">
+              {/* <h4>Routine: {routine.routines.name}</h4> */}
             </div>
           ))}
         </div>
-        <div>
-          <h4>My Routines:</h4>
-          {profile.myRoutines?.map((myRoutine, index) => (
-            <div key={myRoutine._id} className="profile-my-routine">
-              <h4>Routine: {myRoutine.routine.name}</h4>
-              <p>Frequency: {myRoutine.frequency}</p>
-              <p>Days: {myRoutine.days.join(", ")}</p>
-              <p>Start Time: {myRoutine.startTime}</p>
-            </div>
-          ))}
-        </div>
-        <div>
-          <h4>My Activities:</h4>
-          {profile.activities?.map((activity, index) => (
-            <div key={activity._id} className="profile-activity">
-              <h4>Name: {activity.name}</h4>
-              <p>Description: {activity.description}</p>
-              <p>Routines: {activity.routines.map((routine, index) => (
-                <span key={routine._id} className="profile-routine">{routine.name}</span>
-              ))}</p>
-            </div>
-          ))}
-        </div>
-        <button onClick={logout}>Logout</button>
       </div>
-    </div>
   );
 };
 
